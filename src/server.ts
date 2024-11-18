2fbd95640a241466e924ba0733ca9fa61097c3e3
Phát Trần <tranphat701@gmail.com>
2024-11-18 12:41:34 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
 const port = env.PORT;
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
 mongoose
   .connect(env.MONGODB_CONNECTION_STRING)
   .then(() => {
     console.log("connection mongodb success!");
-    app.listen(port, () => {
-      console.log(`server running on http://localhost:${port}`);
-    });
   })
   .catch((error) => console.log(error));
